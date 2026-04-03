11566f978268347192bbed36097ec5f6b928b550
Max <max@MacBook-Pro-Max.local>
2026-04-03 22:42:45 +0300

    feat: add GitHub Pages deployment and fix paths

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'preflight-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
